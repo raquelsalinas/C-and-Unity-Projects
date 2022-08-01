d4814d071de16fc23679987b6d8e76fd13dab526
raquelsalinas <106935808+raquelsalinas@users.noreply.github.com>
2022-08-01 17:28:28 -0500

    Fixing assignment

--- Basic_C#_Programs/Boolean Logic Submission Assignment/Program.cs
+++ Basic_C#_Programs/Boolean Logic Submission Assignment/Program.cs
@@ -14,7 +14,7 @@ class Program {
         string ticketStatus = Console.ReadLine();
         int ticket = Convert.ToInt32(ticketStatus);
 
-        bool qualified = (age >= 15 && DUI == "false" && ticket <= 3);
+        bool qualified = (age > 15 && DUI == "false" && ticket <= 3);
         Console.WriteLine("Qualified?");
         Console.WriteLine(qualified);
         Console.ReadLine();

6e84d1d8ae69e51e02c63c716730e332a9b69565
raquelsalinas <106935808+raquelsalinas@users.noreply.github.com>
2022-08-01 23:19:11 -0500

    Submitting assignment

---
+++ Basic_C#_Programs/Boolean Comparison Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+       
+        int i = 5;
+        int x = 6;
+        
+        while (i < 10) {
+            Console.WriteLine("Hello, World!");
+            i++;
+        }
+
+        do{
+            Console.WriteLine("Hello again, World!");
+            x++;
+        } while (x < 10);
+    }
+}
+

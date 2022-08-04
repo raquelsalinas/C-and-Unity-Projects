5b8ae652386258ad674d61349ee15ba7703c7fe7
raquelsalinas <106935808+raquelsalinas@users.noreply.github.com>
2022-08-04 02:26:37 -0500

    Submitting assignment

--- Basic_C#_Programs/Iteration/Program.cs
+++ Basic_C#_Programs/Iteration/Program.cs
@@ -5,16 +5,56 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Part 1
         Console.WriteLine("Please input some text: ");
         string userInput = Console.ReadLine();
 
-        List<string> pets = new List<string>() { "cat", "dog", "bird", "fish" };
+        string[] Languages = new string[4];
+        Languages[0] = "C#";
+        Languages[1] = "Java";
+        Languages[2] = "C++";
+        Languages[3] = "HTML";
 
-        foreach (string pet in pets)
+        for (int x = 0; x <= 4; x++)
         {
-            pets.Add(userInput);
+            Console.Write(Languages[x] + userInput);
         }
-        Console.WriteLine(pets);
-        Console.ReadLine();
+
+        //Part 2
+        while (true)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        //Part 3
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        for (int j = 0; j <= 10; j = j + 1)
+        {
+            Console.WriteLine(j);
+        }
+
+        //Part 4
+        Console.WriteLine("Please write a the name of a big city in the United States.");
+        string userCity = Console.ReadLine();
+
+        List<string> cities = new List<string>() { "Chicago", "New York", "Miami", "Boston" };
+
+        foreach (string city in cities)
+        {
+            if(city == userCity)
+            {
+                Console.WriteLine(city);
+                Console.ReadLine();
+                Environment.Exit(0);
+            } else
+            {
+                Console.WriteLine("Sorry, this city isn't in the list. Try again.");
+                return;
+            }
+        }
     }
 }

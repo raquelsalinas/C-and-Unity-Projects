5dd70838fd150acf2558295a22fcc3396b3c403c
raquelsalinas <106935808+raquelsalinas@users.noreply.github.com>
2022-08-06 02:06:07 -0500

    Submitting assignment

---
+++ Basic_C#_Programs/TwentyOne/Operations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Operations
+    {
+        public int Add(int x)
+        {
+            int total;
+            total = x + 5;
+            return total;
+        }
+
+        public int Subtract(int y)
+        {
+            int total2;
+            total2 = y - 5;
+            return total2;
+        }
+
+        public int Multiply(int z)
+        {
+            int total3;
+            total3 = z + 5;
+            return total3;
+        }
+    }
+}
---
+++ Basic_C#_Programs/TwentyOne/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            var userInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Add(userInput), Subtract(userInput), Multiply(userInput));
+            Console.ReadLine();
+        }   
+    }
+}

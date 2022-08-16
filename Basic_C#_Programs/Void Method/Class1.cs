6ba49156d363fd5c7a0b549a047c40f5d730fb32
raquelsalinas <106935808+raquelsalinas@users.noreply.github.com>
2022-08-15 20:58:38 -0500

    Submitting assignment

---
+++ Basic_C#_Programs/Void Method/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method
+{
+    public class Class1
+    {
+        public void Operation(int firstNumber, int secondNumber)
+        {
+            int total = firstNumber + 5;
+            Console.WriteLine(total + "," + secondNumber);
+        }
+    }
+}
---
+++ Basic_C#_Programs/Void Method/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 myclass = new Class1();
+
+            myclass.Operation(2, 3);
+            
+            myclass.Operation(secondNumber: 10, firstNumber: 12);
+
+            Console.ReadLine();
+        }
+    }
+}

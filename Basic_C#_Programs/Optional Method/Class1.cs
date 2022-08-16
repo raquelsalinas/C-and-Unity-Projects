4140a3c1ff6eba1e57f4ec13969b6f2c7ac87682
raquelsalinas <106935808+raquelsalinas@users.noreply.github.com>
2022-08-15 19:56:03 -0500

    Submitting assignment

---
+++ Basic_C#_Programs/Optional Method/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Method
+{
+    public class Class1
+    {
+        public int Operation(int a, int b)
+        {
+            int result;
+            result = a + b;
+            return result;
+        }
+    }
+}
---
+++ Basic_C#_Programs/Optional Method/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Method
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 myclass = new Class1();
+
+            Console.WriteLine("Enter a value:");
+            int valueOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter another value or leave space blank and hit enter:");
+            string userInput = Console.ReadLine();
+            int valueTwo = 5;
+            if (userInput != "")
+            {
+                valueTwo = Convert.ToInt32((userInput));
+
+            }
+
+            Console.WriteLine(myclass.Operation(valueOne, valueTwo));
+            Console.ReadLine();
+        }
+    }
+}

8961d1a95a17983fff200ae933689a1e935cdcb4
raquelsalinas <106935808+raquelsalinas@users.noreply.github.com>
2022-08-16 16:46:51 -0500

    Submitting assignment

---
+++ basic_c#_programs/methods and objects/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Objects
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+    }
+}
---
+++ basic_c#_programs/methods and objects/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Objects
+{
+    public abstract class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public abstract void SayName();
+       
+    }
+}

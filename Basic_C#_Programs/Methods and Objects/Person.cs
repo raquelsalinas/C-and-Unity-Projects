6798fc12b967abdddeccee8c659333947591b56f
raquelsalinas <106935808+raquelsalinas@users.noreply.github.com>
2022-08-16 02:50:28 -0500

    Submitting assignment

---
+++ Basic_C#_Programs/Methods and Objects/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Objects
+{
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: ");
+        }
+    }
+}
---
+++ Basic_C#_Programs/Methods and Objects/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee myEmployee = new Employee();
+
+            myEmployee.firstName = "Sample";
+            myEmployee.lastName = "Student";
+            myEmployee.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}

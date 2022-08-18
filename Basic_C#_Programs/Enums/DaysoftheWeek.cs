852971ab526d2a45741fbf7dde39f3212077ba28
raquelsalinas <106935808+raquelsalinas@users.noreply.github.com>
2022-08-18 02:58:42 -0500

    Submitting assignment

---
+++ Basic_C#_Programs/Enums/DaysoftheWeek.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    public class DaysoftheWeek
+    {
+        public Days theDay { get; set; }
+    }
+
+        public enum Days
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
---
+++ Basic_C#_Programs/Enums/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DaysoftheWeek theDays = new DaysoftheWeek();
+                Console.WriteLine("Please type the day of the week.");
+                string userInput = Console.ReadLine();
+
+                var day = (Days)Enum.Parse(typeof(Days), userInput);
+
+                Console.WriteLine("Today is {0}. ", (Days)Enum.Parse(typeof(Days), userInput));
+                Console.ReadLine();
+            }
+            catch (SystemException e)
+            {
+                Console.WriteLine("Please enter an actual day of the week. Thank you.");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}

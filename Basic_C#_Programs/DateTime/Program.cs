6719fa4dd82ff409fd776c122449a954c6817aad
raquelsalinas <106935808+raquelsalinas@users.noreply.github.com>
2022-08-20 22:42:44 -0500

    Submitting assignment

---
+++ Basic_C#_Programs/DateTime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace DateTimeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("Please enter a number:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(DateTime.Now.AddHours(userInput));
+
+            Console.ReadLine();
+        }
+    }
+}
